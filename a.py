991b076b2e13e150c61f3f7281c3a7803c2a6fc9
Fabio0306 <fabitojustitz@gmail.com>
2025-06-21 11:59:44 -0300

    Create diary

---
+++ a.py
@@ -0,0 +1,26 @@
+import random
+
+emoji = random.choice(["😎", "😊", "😢", "😡", "🤣", "💪", "👌", "👍", "🥺", "😁"])
+
+print(emoji)
+
+if emoji == "😎":
+    print("Cool emoji!")
+elif emoji == "😊":
+    print("Happy emoji!")
+elif emoji == "😢":
+    print("Sad emoji!")
+elif emoji == "😡":
+    print("Angry emoji!")
+elif emoji == "🤣":
+    print("Funny emoji!")
+elif emoji == "💪":
+    print("Strong emoji!")
+elif emoji == "👌":
+    print("Perfect emoji!")
+elif emoji == "👍":
+    print("Like emoji!")
+elif emoji == "🥺":
+    print("Pleading emoji!")
+elif emoji == "😁":
+    print("Grinning emoji!")
